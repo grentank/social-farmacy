46e59901cc3d8be50067905386f9e08c6e3b3771
Scoutcallie <79151114057@mail.ru>
2025-06-11 14:14:32 +0300

    createDB

---
+++ server/db/migrations/20250611104835-create-user.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.TEXT
+      },
+      email: {
+        type: Sequelize.TEXT
+      },
+      password: {
+        type: Sequelize.TEXT
+      },
+      admin: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("NOW"),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("NOW"),
+
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
---
+++ server/db/models/sale.js
@@ -0,0 +1,18 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+ const Sale = sequelize.define('Sale', {
+ id: { type: DataTypes.BIGINT, primaryKey: true, autoIncrement: true },
+ user_id: { type: DataTypes.BIGINT, allowNull: false },
+ product_id: { type: DataTypes.BIGINT, allowNull: false },
+ date_start: { type: DataTypes.DATE, allowNull: false },
+ date_end: { type: DataTypes.DATE }
+ }, {
+ tableName: 'Sales',
+ timestamps: false
+ });
+ Sale.associate = function(models) {
+ Sale.belongsTo(models.User, { foreignKey: 'user_id' });
+ Sale.belongsTo(models.Product, { foreignKey: 'product_id' });
+ };
+ return Sale;
+};
