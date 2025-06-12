d560498c0a4554a548145e43c8d36d102261c8c2
Scoutcallie <79151114057@mail.ru>
2025-06-12 20:50:20 +0300

    StyleForm

--- client/src/pages/AuthPage.jsx
+++ client/src/pages/AuthPage.jsx
@@ -5,7 +5,7 @@ import Button from "react-bootstrap/Button";
 import { UserApi } from "../services/UserApi";
 import AuthButton from "../components/AuthButton/AuthButton";
 import { Navigate, useNavigate } from "react-router";
-
+import "./AuthPage.css";
 export default function AuthPage({setCurrentUser, currentUser}) {
   const [form, setForm] = useState({ name: "", email: "", password: "" });
   const [isLogin, setIsLogin] = useState(true);
@@ -42,26 +42,26 @@ export default function AuthPage({setCurrentUser, currentUser}) {
     }
   };
 
-  const backgroundStyle = {
-    backgroundImage: "url('https://png.pngtree.com/thumb_back/fh260/background/20240720/pngtree-tablets-in-bulk-are-multicolored-on-a-blue-background-the-concept-image_15902349.jpg')",
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    backgroundRepeat: "no-repeat",
-    minHeight: "55vh",
-    padding: "20px"
-  };
+  // const backgroundStyle = {
+  //   backgroundImage: "url('https://png.pngtree.com/thumb_back/fh260/background/20240720/pngtree-tablets-in-bulk-are-multicolored-on-a-blue-background-the-concept-image_15902349.jpg')",
+  //   backgroundSize: "cover",
+  //   backgroundPosition: "center",
+  //   backgroundRepeat: "no-repeat",
+  //   minHeight: "55vh",
+  //   padding: "20px"
+  // };
 
-  const formStyle = {
-    backgroundColor: "rgba(255, 255, 255, 0.9)",
-    padding: "20px",
-    borderRadius: "10px",
-    maxWidth: "400px",
-    margin: "0 auto"
-  };
+  // const formStyle = {
+  //   backgroundColor: "rgba(255, 255, 255, 0.9)",
+  //   padding: "20px",
+  //   borderRadius: "10px",
+  //   maxWidth: "400px",
+  //   margin: "0 auto"
+  // };
 
   return (
-    <div style={backgroundStyle}>
-      <div style={formStyle}>
+   <div className="auth-bg">
+  <div className="auth-form-wrap">
         <h2>{isLogin ? "Вход" : "Регистрация"}</h2>
         <Form onSubmit={handleSubmit}>
           {!isLogin && (
