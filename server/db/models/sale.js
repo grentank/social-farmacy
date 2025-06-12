6a8913e4d780a9c9ee75eefb3383651d20134b98
Scoutcallie <79151114057@mail.ru>
2025-06-12 12:17:40 +0300

    fixDb

--- server/db/models/sale.js
+++ server/db/models/sale.js
@@ -17,5 +17,5 @@ module.exports = (sequelize, DataTypes) => {
  return Sale;
 };
 
-On branch server
-Your branch is up to date with 'origin/server'.
+// On branch server
+// Your branch is up to date with 'origin/server'.
